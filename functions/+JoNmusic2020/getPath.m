b21cc52316e25b5ddec00873706907475afcfb45
Octave <octave.etard@gmail.com>
2020-10-20 13:08:36 +0100

    Updated documentation

--- functions/+JoNmusic2020/getPath.m
+++ functions/+JoNmusic2020/getPath.m
@@ -5,30 +5,55 @@ function topFolder = getPath(varargin)
 % Author: Octave Etard
 %
 % This functions defines the base folder where the data to load, or results
-% to save are located
+% to save are located.
+%
+% Currently assuming the following architecture:
+%
+% someFolder
+% |--- JoNmusic2020.git                 (arbitray name)
+% |    |--- someFolder
+% |         |--- main analysis scripts
+% |--- data                             (location of 'dataFolder')
+%      |--- EEG
+%      |    |--- processed
+%      |--- stimuli
+%      |    |--- features
+%      |    |--- rawInventions
+%      |--- behav
+%
+% location of the base folder containing the data ; % change to 
+% 'path/to/top/folder/containing/data' if a different location is desired.
+dataFolder = fullfile('..','..','data');
+%
+% See 'JoNmusic2020.makePathEEGFolder' 'JoNmusic2020.makePathEEGFolder'
+% JoNmusic2020.'makePathFeatureFiles' JoNmusic2020.'makePathSaveResults'
+% for the functions specifying the expected organisation of the data folder
+%
+% See 'JoNmusic2020.makeNameEEGDataFile' 'JoNmusic2020.makeNameEEGDataFile'
+% for the functions specifying the expected file names.
 %
 switch varargin{1}
     
     case 'linearModelsResults'
         % where to save results, or load them from to plot
-        topFolder = fullfile('..','..','data','linearModelsResults');
+        topFolder = fullfile(dataFolder,'linearModelsResults');
         
     case 'EEG'
         % where the EEG data is located
         % varargin{2}: 'raw' or 'processed'
-        topFolder = fullfile('..','..','data','EEG',varargin{2});
+        topFolder = fullfile(dataFolder,'EEG',varargin{2});
         
     case 'features'
         % where the stimulus features are located
-        topFolder = fullfile('..','..','data','stimuli','features');
+        topFolder = fullfile(dataFolder,'stimuli','features');
         
     case 'MIDI'
         % where the MIDI files are located
-        topFolder = fullfile('..','..','data','stimuli','rawInventions');
+        topFolder = fullfile(dataFolder,'stimuli','rawInventions');
         
     case 'behav'
         % where the stimulus features are located
-        topFolder = fullfile('..','..','data','behav');
+        topFolder = fullfile(dataFolder,'behav');
         
     otherwise
         error('Unrecognised option.')
